55fa4c48b416d7f6b32a06734f7c910b9e9db2dd
Tomek Tarabasz <tomektarabasz@gmail.com>
2019-01-11 01:51:30 +0100

    Add project files.

---
+++ ScaningToTxt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+
+
+namespace ScaningToTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] paths = { @"C:\Księgowa2019\Skany\Faktura1.jpg" };
+            Console.WriteLine("To jest argument args[0]={0}", args[0]);
+           
+            Console.ReadKey();
+        }
+    }
+}
